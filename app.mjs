085743533fc95b136e323f80a8698f20102a0f0a
Paul <awake.early@gmail.com>
2023-03-28 12:13:19 -0500

    response working, verifying client side input

--- app.mjs
+++ app.mjs
@@ -12,8 +12,24 @@ app.use(bp.json())
 app.use(logevents)   //middleware that logs server events (https://www.npmjs.com/package/morgan)
 
 app.get("/", (req, res) => {
-    res.send("server listening, hello world");
+    res.sendFile("C:\\Users\\Paul\\JSNoodling\\RidgeviewSignExpress\\html\\index.html")
    });
+
+app.get("/css", (req, res) => {
+  res.sendFile("C:\\Users\\Paul\\JSNoodling\\RidgeviewSignExpress\\css\\default.css")
+});
+
+app.get("/script", (req, res) => {
+  res.sendFile("C:\\Users\\Paul\\JSNoodling\\RidgeviewSignExpress\\scripts\\index.js")
+});
+
+app.post("/verify", (req, res) => {
+  console.log("L1", [req.body.L1, req.body.L2, req.body.L3], [req.body.L4, req.body.L5, req.body.L6]);
+  sign.initSign([[req.body.L1, req.body.L2, req.body.L3], [req.body.L4, req.body.L5, req.body.L6]]);
+  let lengthVerify = sign.validateLineLengths();
+  let lettersVerify = sign.validateLettersAvailable();
+  res.send({"length": lengthVerify, "letters": lettersVerify})
+});
 
 app.listen(port, () => {
     console.log("Express server listning on port " + port);
@@ -21,3 +37,8 @@ app.listen(port, () => {
 
 //use nodemon app.js to start
 //or use node app.js to start
+
+
+
+
+
